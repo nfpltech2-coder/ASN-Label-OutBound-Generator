8ba7933d5bfa62343b153be50792f97e0fdf9b76
nfpltech2 <nfpl.tech2@gmail.com>
2026-02-16 12:39:44 +0530

    Cleanup repository: Remove test data and finalize .gitignore

--- inspect_full_page1.py
+++
@@ -1,7 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open("Invoice.pdf") as pdf:
-    text = pdf.pages[0].extract_text()
-    print("--- FULL PAGE 1 TEXT ---")
-    print(text)
-    print("--- END PAGE 1 TEXT ---")
